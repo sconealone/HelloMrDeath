051cc3fa8c6529f72a07824818ca07c04ff8d30e
Mitch Tee <mitch.tee@gmail.com>
2012-05-12 07:00:34 -0700

    finished the initialization of platforms.

--- Classes/Level.cpp
+++ Classes/Level.cpp
@@ -42,3 +42,45 @@ CCScene* Level::scene() {
 void Level::update(float dt) {
 
 }
+
+
+/**
+ * Converts a size in tiles to a size in metres
+ */
+float tilesToMetres(float tileNumber) {
+	const int PIXELS_PER_TILE = 32;
+	const int PIXELS_PER_METRE = 64; // placeholder
+	return tileNumber * PIXELS_PER_TILE / PIXELS_PER_METRE;
+}
+
+bool Level::isPlatform(CCPoint tileCoord) {
+	return platformLayer->tileGIDAt(tileCoord);
+}
+
+void Level::initPlatformsFromTiledMap() {
+	CCSize mapSize = tiledMap->getMapSize();
+	for (int i = 0; i < mapSize.height; ++i) {
+		for (int j = 0; j < mapSize.width; ++j) {
+			if (isPlatform(ccp(i,j))) {
+				int lastColumn = j;
+				while (lastColumn < mapSize.width && isPlatform(ccp(i,lastColumn))) {
+					++lastColumn;
+				}
+				int width = tilesToMetres(lastColumn - j);
+				int height = tilesToMetres(1);
+				float centerX = tilesToMetres(i + 0.5f);
+				float centerY = tilesToMetres(j) + height / 2.0f;
+				createPlatformBody(width, height, i, j);
+				j = lastColumn;
+			}
+		}
+	}
+}
+
+void Level::createPlatformBody(float width, float height, float centerX, float centerY) {
+	b2BodyDef def;
+	b2Body* body = world->CreateBody(&def);
+	b2PolygonShape platform;
+	b2Vec2 center(centerX, centerY);
+	platform.SetAsBox(width, height, center, 0.0f);
+}
