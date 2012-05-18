c60a743358f7a01780827848568db7e04eb7ca79
Olive <olivia_ny_zhang@hotmail.com>
2012-05-17 17:53:38 -0700

    leave me alone!!!

--- Classes/Level.cpp
+++ Classes/Level.cpp
@@ -22,6 +22,12 @@ bool Level::init() {
 	do {
 		CC_BREAK_IF(!super::init());
 		
+		gameLayer = CCLayer::node();
+		
+		this->addChild(gameLayer, 0);
+		
+		MrDeath *player = new MrDeath();
+		
 		this->schedule(schedule_selector(Level::update), TIMESTEP);
 		initSuccessful = true;
 	} while (0);
@@ -34,11 +40,14 @@ CCScene* Level::scene() {
 	do {
 		scene = CCScene::node();
 		CC_BREAK_IF(!scene);
+		
 		Level* layer = Level::node();
+		
 		CC_BREAK_IF(!layer);
 		scene->addChild(layer);
 	} while(0);
 	return scene;
+
 }
 
 void Level::update(float dt) {
